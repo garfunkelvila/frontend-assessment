17ef59e6abb2355655e70d7eefa2449bf2d24f9d
Garfunkel <garfunkelvila@outlook.com>
2021-06-08 03:47:52 +0800

    Update banana

--- exam.mind-arc/pages/index.vue
+++ exam.mind-arc/pages/index.vue
@@ -8,8 +8,14 @@
     Bonus
     <p>('b' + 'a' + + 'a' + 'a').toLowerCase() is "banana"</p>
     <p>
-      For real I am not sure, the second 'a' (after the two plus signs) is assumed number why it returned 'Not a Number' aka 'NaN' which in lower case is nan<br>
-      combined you got ba'nan'a.<br><br>It seems it is a shorthand for a number parsing.
+      For real I am not sure, the second 'a' (after the two plus signs) is assumed number thats why it returned 'Not a Number' aka 'NaN' which in lower case is nan
+      combined you got ba'nan'a.
+    </p>
+    <p>
+      It seems it is a shorthand for a number parsing because single quote in javascript assumes string and use concat for the enclosed parenthesis.
+    </p>
+    <p>
+      I usually nest it with parenthesis to be more explicit.
     </p>
   </v-container>
 </template>
@@ -18,3 +24,8 @@
 export default {
 }
 </script>
+<style scoped lang="scss">
+p {
+  max-width: 5in;
+}
+</style>
