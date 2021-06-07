03420eefefa80008f21eb45d62a09b00b5c62cfe
Garfunkel <garfunkelvila@outlook.com>
2021-06-08 02:27:59 +0800

    Create pic-card.vue

---
+++ exam.mind-arc/components/exercise1/pic-card.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    style="display: flex; flex-direction: column; height: 100%;"
+  >
+    <v-card-text>
+      <v-card>
+        <v-img
+          src="/images/400x300.png"
+        />
+      </v-card>
+    </v-card-text>
+    <v-card-text class="text-center pt-0" style="height: 100%;">
+      {{ text }}
+    </v-card-text>
+    <v-card-text class="justify-center" style="display: flex;">
+      <v-btn outlined rounded class="pa-4" style="border: 2px solid black;">
+        Read More
+      </v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
