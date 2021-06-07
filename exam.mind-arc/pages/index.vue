aece31e840436807c70ac0916162e451b3754e19
Garfunkel <garfunkelvila@outlook.com>
2021-06-08 03:41:13 +0800

    fix wrong precedence

--- exam.mind-arc/pages/index.vue
+++ exam.mind-arc/pages/index.vue
@@ -8,7 +8,7 @@
     Bonus
     <p>('b' + 'a' + + 'a' + 'a').toLowerCase() is "banana"</p>
     <p>
-      For real I am not sure, the second 'a' (before the two plus signs) is assumed number why it returned 'Not a Number' aka 'NaN' which in lower case is nan<br>
+      For real I am not sure, the second 'a' (after the two plus signs) is assumed number why it returned 'Not a Number' aka 'NaN' which in lower case is nan<br>
       combined you got ba'nan'a.<br><br>It seems it is a shorthand for a number parsing.
     </p>
   </v-container>
