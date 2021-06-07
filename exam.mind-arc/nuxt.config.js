31210c4904e73a9ba47f5596a6888273159c644c
Garfunkel <garfunkelvila@outlook.com>
2021-06-08 02:27:46 +0800

    add composition api

--- exam.mind-arc/nuxt.config.js
+++ exam.mind-arc/nuxt.config.js
@@ -33,7 +33,9 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    // https://composition-api.nuxtjs.org/getting-started/setup
+    '@nuxtjs/composition-api/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
