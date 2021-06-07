ed969f073661f5be3ea68f6e906f4642230ef959
Garfunkel <garfunkelvila@outlook.com>
2021-06-08 03:15:19 +0800

    Update index.vue

--- exam.mind-arc/pages/index.vue
+++ exam.mind-arc/pages/index.vue
@@ -1,6 +1,16 @@
 <template>
   <v-container>
-    Exercize1/Exercize2/Bonus
+    <ol>
+      <li><nuxt-link to="/exercise1">Exercise 1</nuxt-link></li>
+      <li><nuxt-link to="/exercise2">Exercise 2</nuxt-link></li>
+    </ol>
+    <br>
+    Bonus
+    <p>('b' + 'a' + + 'a' + 'a').toLowerCase() is "banana"</p>
+    <p>
+      For real I am not sure, the second 'a' (before the two plus signs) is assumed number why it returned 'Not a Number' aka 'NaN' which in lower case is nan<br>
+      combined you got ba'nan'a.<br><br>It seems it is a shorthand for a number parsing.
+    </p>
   </v-container>
 </template>
 
